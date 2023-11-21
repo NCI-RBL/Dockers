9456287b8c2ea2a84eaf2d023b3177907eaaaca5
wilfriedguiblet <guibletwm@nih.gov>
2023-11-21 00:17:45 -0700

    Fix other saving issues in iCLIP reports

--- recipes/utils/iCLIP_V3/workflow/scripts/AnnotationFormat.py
+++ recipes/utils/iCLIP_V3/workflow/scripts/AnnotationFormat.py
@@ -100,7 +100,7 @@ def MergeDataframes(SameStrandGen_df, SameStrandIntrons_df, SameStrandRMSK_df, S
     #Out_df['Same_intron_LargeOL'] = SameStrandMerged_df['???'] FROM BEDTOOLS INTERSECT?
     #Out_df['Same_exon_LargeOL'] = SameStrandMerged_df['???'] FROM BEDTOOLS INTERSECT?
     Out_df['Same_Repeat'] = SameStrandMerged_df['repClass']
-    Out_df['Same_ncRNA'] = SameStrandMerged_df['ncrnaClass']
+    Out_df['Same_ncRNA'] = SameStrandMerged_df['ncrnaName']
     Out_df['Same_UTR'] = SameStrandMerged_df['UTR']
 
     Out_df['Oppo_ensembl_gene_id'] = OppoStrandMerged_df['gene_id']
